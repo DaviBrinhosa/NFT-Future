b3eb78324806d9f0f71585444505230c2221336c
DaviBrinhosa <davigilvanni@gmail.com>
2022-04-23 13:40:29 -0300

    Update 005

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,29 +18,37 @@ function sideMenuByScroll() {
     } 
 }
 
+const showButton01 = document.getElementById("post__button_01")
+const showButton02 = document.getElementById("post__button_02")
+const showButton03 = document.getElementById("post__button_03")
+const showButton04 = document.getElementById("post__button_04")
+const showBlur01 = document.getElementById("blur_01")
+const showBlur02 = document.getElementById("blur_02")
+const showBlur03 = document.getElementById("blur_03")
+const showBlur04 = document.getElementById("blur_04")
+
 function showButton() {
     if (window.screen.availWidth < 1280) {   
-        if (document.documentElement.scrollTop > 150 && document.documentElement.scrollTop <= 300){
-            document.getElementById("post__button_01").style.display = "block";
-            document.getElementById("blur_01").style.display = "block";
-            document.getElementById("post__button_02").style.display = "block";
-            document.getElementById("blur_02").style.display = "block";
+        if (document.querySelector('.post').getBoundingClientRect().top < 350){
+            showButton01.style.display = "block";
+            showBlur01.style.display = "block";
+            showButton02.style.display = "block";
+            showBlur02.style.display = "block";
+            showButton03.style.display = "block";
+            showBlur03.style.display = "block";
+            showButton04.style.display = "block";
+            showBlur04.style.display = "block";
         } else {
-            document.getElementById("post__button_01").style.display = "none";
-            document.getElementById("blur_01").style.display = "none";
-            document.getElementById("post__button_02").style.display = "none";
-            document.getElementById("blur_02").style.display = "none";
-
-        } if (document.documentElement.scrollTop > 300 && document.documentElement.scrollTop <= 500){
-            document.getElementById("post__button_03").style.display = "block";
-            document.getElementById("blur_03").style.display = "block";
-            document.getElementById("post__button_04").style.display = "block";
-            document.getElementById("blur_04").style.display = "block";
-        } else {
-            document.getElementById("post__button_03").style.display = "none";
-            document.getElementById("blur_03").style.display = "none";
-            document.getElementById("post__button_04").style.display = "none";
-            document.getElementById("blur_04").style.display = "none";
+            showButton01.style.display = "none";
+            showBlur01.style.display = "none";
+            showButton02.style.display = "none";
+            showBlur02.style.display = "none";
+            showButton03.style.display = "none";
+            showBlur03.style.display = "none";
+            showButton04.style.display = "none";
+            showBlur04.style.display = "none";
         }
     }
 }
+
+showButton01.getBoundingClientRect().top
